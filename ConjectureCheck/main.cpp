681c98c0be98f19c7e6e1dafa403c317fac5f0ae
Folling <jonathancoding@gmail.com>
2017-08-19 21:45:25 +0200

    Update main.cpp

--- ConjectureCheck/main.cpp
+++ ConjectureCheck/main.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
-#include <fstream>
 #include <cmath>
 #include <vector>
 #include <conio.h>
 
-std::vector<int> findAllPrimesUntilX(int x);
+// this code requires you to have openMP optimizations enabled
+
+void findAllPrimesUntilX(int x);
 bool checkPrime(int x);
 int sumDeca(int x);
+
 //enter the amount of numbers to check
 const int upToXthPrimes = 10000000;
-std::vector<int> primes = findAllPrimesUntilX(upToXthPrimes);
-int primeCount , threeCount, valIntervCount;
+std::vector<int> primes;
+int primeCount, threeCount, valIntervCount;
 
-int main() {	
+int main() {
+	findAllPrimesUntilX(upToXthPrimes);
 	//checks whether we've reached the intervals as large as the numbers we checked primes for
-	register int prevPrime = 0;
-	register int decaCount = 0;
-	while (decaCount * 100 <= upToXthPrimes) {
-		if (decaCount % 50 == 0) std::cout << "Checked the first " << decaCount << " Intervals!" <<std::endl;
-		for (register int i = prevPrime; i < primes.size(); i++) {
+	int prevPrime = 0;
+	int decaCount = 0;
+	while (decaCount * 10 <= upToXthPrimes) {
+		if (decaCount % 50 == 0) std::cout << "Checked the first " << decaCount << " Intervals!" << std::endl;
+		for (int i = prevPrime; i < primes.size(); i++) {
 			if (primes[i] >= (decaCount + 1) * 10)
 			{
 				prevPrime = i;
 				break;
 			}
-			else if (primes[i] < (decaCount*10)+1) continue;
-			else primeCount++;
+			else if (primes[i] < (decaCount * 10) + 1) continue;
+			primeCount++;
 		}
 		if (primeCount == 3) threeCount++;
 		if (sumDeca(decaCount) % 3) valIntervCount++;
@@ -35,7 +38,7 @@ int main() {
 				primeCount = 0;
 			}
 			else {
-				std::cout << "Found a disproof in the interval of: " << decaCount * 10+1 << " and " << (decaCount+1) * 10 << std::endl;
+				std::cout << "Found a disproof in the interval of: " << decaCount * 10 + 1 << " and " << (decaCount + 1) * 10 << std::endl;
 				_getch();
 				exit(1);
 			}
@@ -43,9 +46,9 @@ int main() {
 		else {
 			decaCount++;
 		}
-	}	
+	}
 	std::cout << "No disproof found!\n";
-	std::cout << "Intervals checked: " << decaCount-1 << std::endl;
+	std::cout << "Intervals checked: " << decaCount - 1 << std::endl;
 	std::cout << "Primes used: " << primes.size() << std::endl;
 	std::cout << "Numbers checked: " << upToXthPrimes << std::endl;
 	std::cout << "Intervals with three primes: " << threeCount << std::endl;
@@ -54,34 +57,34 @@ int main() {
 }
 
 //returns all primes until x in a vector
-std::vector<int> findAllPrimesUntilX(int x) {
-	std::vector <int> result;
-	for (register int i = 1; i <= x; i++) {
+void findAllPrimesUntilX(int x) {
+	for (int i = 1; i <= x; i++) {
 		if (i % 500 == 0) std::cout << "The first " << i << " numbers have been checked for whether they're prime.\n";
-		if (checkPrime(i)) result.push_back(i);
+		if (checkPrime(i)) primes.push_back(i);
 	}
 	std::cout << "All primes have been calculated!\n";
-	return result;
 }
 
 //checks whether a number is prime
-bool checkPrime(register int n) {
+bool checkPrime(int n) {
+	int s = sqrt(n);
 	if (n == 2) return true;
 	if (n < 2 || n % 2 == 0) return false;
+	bool prime = true;
 
-	for (register int i = 3; i<=sqrt(n); i += 2) {
-		if (n % i == 0) return false;
+#pragma omp parallel for
+	for (int i = 3; i <= s; i += 2) {
+		if (n % i == 0) prime = false;
 	}
-	return true;
+
+	return prime;
 }
 
 //takes an input and sums it up until it reaches the next 10th e.g. 10+11+12+..+19
 int sumDeca(int x) {
 	int sum = 0;
-	for (int current = (x * 10)+1; current <= (x+1) * 10; current++) {
+	for (int current = (x * 10) + 1; current <= (x + 1) * 10; current++) {
 		sum += current;
 	}
-	return sum+1;
+	return sum + 1;
 }
-
-//I apologise if the commenting on this code is not ideal, if you have any questions feel free to ask
